a9c1bc711e5549ef815bc5fa6d04c8287b2f0d3f
Annica <45278655+chiuannica@users.noreply.github.com>
2021-04-06 02:18:37 -0400

    added select uni filters that doesn't work and hindex filter

--- professor_citations_app.py
+++ professor_citations_app.py
@@ -4,6 +4,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+# *===========================*
+# *===* Intro Information *===*
+# *===========================*
+
+
 st.title("Computer Science Department Professors' Citations")
 st.subheader("Purpose of Data")
 st.write("This is the display of data I spent months collecting as a research assistant.")
@@ -22,6 +27,9 @@ st.markdown('''<ul>
                 <li>The graphs are filtered by the sidebar fields</li>
             </ul>''', unsafe_allow_html=True)
 
+# *======================*
+# *===* LOADING DATA *===*
+# *======================*
 
 # load data
 path = "Crawled_Faculty_data_3.25.2021.csv"
@@ -34,11 +42,13 @@ df_limited = df
 # limit the df to some columns
 df_limited = df_limited[["first", "last", "university", "citations", "h-index", "t10", "rank"]]
 
-# show df
-st.subheader("Data")
+# show df (all of it)
+st.subheader("Raw Full Data")
 st.write(df_limited)
 
-# --- Univ
+# *=========================*
+# *===* SIDEBAR FILTERS *===*
+# *=========================*
 
 # --- CITATION SLIDERS --- 
 # citation sliders
@@ -70,6 +80,21 @@ df_limited = df_limited[df_limited.t10 <= max_t10_slider]
 df_limited = df_limited[df_limited.t10 >= min_t10_slider]
 
 
+# --- h-index SLIDERS ---
+# h-index  sliders
+df_h = df["h-index"]
+
+min_h = int(df_h.min())
+max_h = int(df_h.max())
+
+st.sidebar.write('**Adjust h-index values**')
+max_h_slider = st.sidebar.slider('Adjust the slider to the maximum h-index value', min_value=min_h, max_value=max_h, value=max_h)
+min_h_slider = st.sidebar.slider('Adjust the slider to the minimum h-index value', min_value=min_h, max_value=max_h)
+
+# limit the df based on the sliders
+df_limited = df_limited[df_limited["h-index"] <= max_h_slider]
+df_limited = df_limited[df_limited["h-index"] >= min_h_slider]
+
 # --- RANK MULTISELECT ---
 # select rank
 st.sidebar.write('**Professor Rank**')
@@ -86,9 +111,23 @@ elif (rank_radio == "Assistant"):
     df_limited = df_limited[ (df_limited['rank'] == "Assistant") ]
 
 
+# --- UNIVERSITY MULTISELECT --- 
+st.sidebar.write('**Select Universities**')
+uni_options = df_limited['university']
+uni_multiselect = st.sidebar.multiselect("Select Universities. ", options=["Carnegie Mellon University", "Temple University", "University of Pennsylvania"]);
+#uni_multiselect = st.sidebar.multiselect("Select Universities. ", options=uni_options);
+# !!!! important, this is broken, lol!
 
+if (uni_multiselect):
+    df_limited = df_limited.loc[df_limited['university'].str.contains(r'\b(?:{})\b'.format('|'.join(uni_multiselect)))]
+st.subheader("Filtered dataset")
+st.write(df_limited)
 
-# --- DISPLAYING GRAPHS ---
+# *===========================*
+# *===* DISPLAYING GRAPHS *===*
+# *===========================*
+
+# --- BAR CHARTS ---
 # display bar graph of universities 
 st.subheader("Bar graph of the number of records at each university")
 df_uni_counts = df_limited["university"].value_counts()
@@ -126,6 +165,13 @@ df_citation_uni_counts = df_limited[["citations", "university"]]
 
 c = alt.Chart(df_citation_uni_counts).mark_circle().encode(x='citations', y='university', size='citations', color='citations', tooltip=['citations', 'university', 'citations'])
 st.altair_chart(c, use_container_width=True)
+
+st.subheader("h-index to university")
+df_h_uni_counts = df_limited[["h-index", "university"]]
+
+c = alt.Chart(df_h_uni_counts).mark_circle().encode(x='h-index', y='university', size='h-index', color='h-index', tooltip=['h-index', 'university', 'h-index'])
+st.altair_chart(c, use_container_width=True)
+
 
 
 
